b74f9e7fe27e17255e9eb922a3d0f1b765ee0c7d
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-01 03:42:13 +0500

    .

---
+++ practice2/if_else/if_else.py
@@ -0,0 +1,36 @@
+#1
+a = 20
+b = 10
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+#2
+number = 5
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+  
+#3
+age = 16
+if age >= 18:
+    print("adult")
+else:
+    print("teenager")
+    
+#4
+password = "abc"
+if password == "123":
+    print("right")
+else:
+    print('wrong')
+    
+#5
+a = 20
+b = 10
+if b > a:
+  print(b)
+else:
+  print(a)
