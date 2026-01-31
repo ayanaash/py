193023e506c9be7692a1454c468ab1eaabdd6be5
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-01 03:54:00 +0500

    .

---
+++ practice2/if_else/if_elif_else.py
@@ -0,0 +1,55 @@
+#1
+a = 11
+b = 11
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+  
+#2
+score = 85
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+  
+#3
+age = 25
+if age < 13:
+  print("child")
+elif age < 20:
+  print("teenager")
+elif age < 65:
+  print("adult")
+elif age >= 65:
+  print("senior")
+  
+#4
+day = 4
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
+  
+#5
+x = -3
+if x > 0:
+    print('positive')
+elif x == 0:
+    print('zero')
+else:
+    print('negative')
