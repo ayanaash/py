32831d28d751eb553e4d1bacc785a76b598e57e1
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-01 03:14:12 +0500

    .

---
+++ practice2/boolean/boolean_operators.py
@@ -0,0 +1,20 @@
+#1
+age = 17
+print(age > 12 and age < 18) #True(and: both true)
+
+#2
+sc = 95
+print(sc > 90 or sc < 50) #True(or: if at least one true)
+
+#3
+a = 7
+print(not (a == 13)) #TRue(not: обратное)
+
+#4
+a = 12
+print(not( a % 2 ==0 and a % 3 == 0)) #False
+
+#5
+sign = True
+log = False
+print(sign and not log) #true
