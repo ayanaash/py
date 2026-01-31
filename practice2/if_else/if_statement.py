0bad2d2273bc69adeab79048ac8e98ae1c468e08
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-01 03:24:22 +0500

    .

---
+++ practice2/if_else/if_statement.py
@@ -0,0 +1,26 @@
+#1
+a = 95
+b = 100
+if b > a:
+  print("b is greater than a")
+  
+#2
+number = 10
+if number > 0:
+  print("The number is positive")
+  
+#3
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+  
+#4
+name = "ayana"
+if name == "ayana":
+    print("its true")
+    
+#5
+a = 5
+b = 30
+if a*6 == b:
+    print("that's right")
