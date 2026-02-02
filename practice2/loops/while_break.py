a27c4ba2fdb596231419ebe6912d5e94e9b86fe8
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-02 19:52:47 +0500

    .

---
+++ practice2/loops/while_break.py
@@ -0,0 +1,35 @@
+#1
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1   #1, 2, 3
+  
+#2
+x = 10
+while x > 0:
+    print(x)
+    if x == 7:
+        break
+    x -= 1  #10, 9, 8, 7
+    
+#3
+x = 0
+while x < 5:
+    if x == 2:
+        break
+    print(x)
+    x += 1   #0, 1
+    
+#4
+n = 1
+while n <= 3:
+    print("Python")
+    break   #'python'
+
+#5
+i = 0
+while i < 5:
+    print("Hello")
+    break   #'hello'
