c6189d78d9fbeb94a622d1b70783347d807263fe
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-02 17:31:29 +0500

    .

---
+++ practice2/if_else/short_hand_if.py
@@ -0,0 +1,20 @@
+#1
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#2
+a = 5
+print('yes') if a > 3 else print('no')
+
+#3
+n = 4
+print('even') if n % 2 == 0 else print('odd')
+
+#4
+is_raining = True
+print('take umbrella') if is_raining else print('no umbrella')
+
+#5
+age = 18
+print('adult') if age >= 18 else print('child')
