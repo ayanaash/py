415652671c346b43eade75de30aa6ee60b918538
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-02 19:39:57 +0500

    .

---
+++ practice2/loops/while_loop.py
@@ -0,0 +1,29 @@
+#1
+i = 1
+while i < 6:
+  print(i)
+  i += 1     #0, 1, 2, 3, 4, 5
+  
+#2
+i = 5
+while i > 0:
+    print(i)
+    i -= 1    #5, 4, 3, 2, 1
+
+#3
+i = 0
+while i < 3:
+    print("Hello")
+    i += 1
+
+#4
+i = 2
+while i <= 10:
+    print(i)
+    i += 2
+
+#5
+num = 1
+while num <= 3:
+    print(num * 2)
+    num += 1
