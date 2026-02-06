5fefcf85734372ef8c81687229b1f407a9a94e5a
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-07 02:40:11 +0500

    .

---
+++ practice3/functions/basic_functions.py
@@ -0,0 +1,30 @@
+#1
+def greet():
+ print('hello')
+greet()
+
+#2
+def sum(a, b):
+    return a+b
+print(sum(2, 7))
+
+#3
+def greet(name):
+    print('Hello,', name)
+
+greet('Ayana')
+
+#4
+def even(n):
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+even(32)
+
+#5
+def square(x):
+    return x * x
+
+print(square(6))
