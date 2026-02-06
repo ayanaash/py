ab4b088f8b77cfd79d91fa1e834175eb6943859c
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-07 03:09:42 +0500

    .

---
+++ practice3/functions/function_arguments.py
@@ -0,0 +1,27 @@
+#1
+def my_function(fname):
+  print(fname + " hii")
+my_function("ayana")
+my_function('anaya')
+
+#2
+def subt(a, b):
+    return a - b
+print(subt(29, 12))
+
+#3
+def mult(a, b):
+    return a * b
+print(mult(5, 11))
+
+#4
+def max(a, b):
+    if a > b:
+        return a
+    return b
+print(max(120, 12))
+
+#5
+def length(s):
+    return len(s)
+print(length('hello'))
