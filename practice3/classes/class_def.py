d404f6d30928f9cfc4bc1b0a9523d72f9326c53d
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-11 00:47:52 +0500

    .

---
+++ practice3/classes/class_def.py
@@ -0,0 +1,33 @@
+#1
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)
+
+#2
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+#3
+class Person:
+  pass
+
+#4
+class person:
+    name = "Ayana"
+d = person()
+print(d.name)
+
+#5
+class person:
+    age = 17
+d = person()
+print(d.age)
