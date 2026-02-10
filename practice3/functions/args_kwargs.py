d9d6cc8a68721df24c67466db241a8f6a078ec3c
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-11 00:05:15 +0500

    .

---
+++ practice3/functions/args_kwargs.py
@@ -0,0 +1,28 @@
+#1
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")    # '*' можно писать несколько
+
+#2
+def my_function(greeting, *names):
+  for name in names:
+    print(greeting, name)
+
+my_function("Hello", "Emil", "Tobias", "Linus")
+
+#3
+def f(**kwargs):
+    print(kwargs)
+f(a=1, b=2)   #{'a': 1, 'b': 2}
+
+#4
+def f(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
+f(x=10, y=20)   #x 10, y 20
+
+#5
+def f(**data):
+    print(kwargs.get('name'))
+f(name = 'Ayana', age = 17)  #'Ayana'
