fff1185e3ddaf181505c8d590812ecf5b9e56a7f
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-10 23:00:13 +0500

    .

---
+++ practice3/functions/return_values.py
@@ -0,0 +1,29 @@
+#1
+def my_function(x, y):
+  return x + y
+
+result = my_function(5, 3)
+print(result)
+
+#2
+def subt(a, b):
+    return a - b
+print(subt(29, 12))
+
+#3
+def mult(a, b):
+    return a*b
+res = mult(6, 7)
+print(res)
+
+#4
+def max(a, b):
+    if a > b:
+        return a
+    return b
+print(max(120, 12))
+
+#5
+def length(s):
+    return len(s)
+print(length('hello'))
