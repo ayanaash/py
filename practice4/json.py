03bd9b91f3220111dec1f8199e2600662b5e5e43
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-25 12:36:07 +0500

    .

---
+++ practice4/json.py
@@ -0,0 +1,59 @@
+#1
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])   #30
+
+
+#2
+import json
+
+data = {"name": "Ayana", "age": 16, "city": "Almaty"}
+
+print(json.dumps(data))
+
+
+#3
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+
+
+#4
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+
+#5
+import json
+
+data = {"isStudent": True, "hasCar": False}
+
+print(json.dumps(data))
+#{"isStudent": true, "hasCar": false}
