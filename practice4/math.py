c939fe143359b434cc0e85912aca8cb1f97f4a81
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-25 12:20:40 +0500

    .

---
+++ practice4/math.py
@@ -0,0 +1,38 @@
+#1
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+
+#2
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+#3
+import math
+
+x = math.pi
+
+print(x)
+
+
+#4
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+#5
+x = pow(4, 3)
+
+print(x)
