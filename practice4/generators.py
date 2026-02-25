cb992d52af0c5672dba6f39149d092c8d985f189
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-02-25 12:07:32 +0500

    .

---
+++ practice4/generators.py
@@ -0,0 +1,76 @@
+#1
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))  #1, 2, 3
+ 
+
+#2
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)    #1 - 20
+
+
+#3
+def count_up_to(n):
+  count = 1
+  while count <= n:
+    yield count
+    count += 1
+
+for num in count_up_to(5):
+  print(num)  
+  
+
+#4
+def fibonacci():
+  a, b = 0, 1
+  while True:
+    yield a
+    a, b = b, a + b
+
+# Get first 100 Fibonacci numbers
+gen = fibonacci()
+for _ in range(100):
+  print(next(gen))
+
+
+#5
+def my_gen():
+  try:
+    yield 1
+    yield 2
+    yield 3
+  finally:
+    print("Generator closed")
+
+gen = my_gen()
+print(next(gen))
+gen.close() 
