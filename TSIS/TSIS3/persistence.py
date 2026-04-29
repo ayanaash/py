34fc5cea7144974b4b8abe024113c9a88a2337c5
Ayana <a_shaikhaeslyam@kbtu.kz>
2026-04-29 13:29:36 +0500

    .

--- TSIS/TSIS3/persistence.py
+++ TSIS/TSIS3/persistence.py
@@ -6,7 +6,7 @@ SCORES_FILE = "leaderboard.json"
 
 
 def load_settings():
-    if not os.path.exists(SETTINGS_FILE):
+    if not os.path.exists(SETTINGS_FILE):   #проверка сущ файла
         return {"sound": True, "car_color": (0, 0, 255), "difficulty": "normal"}
     with open(SETTINGS_FILE, "r") as f:
         return json.load(f)
@@ -17,17 +17,17 @@ def save_settings(settings):
         json.dump(settings, f)
 
 
-def load_scores():
+def load_scores():   #список рекордов
     if not os.path.exists(SCORES_FILE):
-        return []
+        return []   #пустой
     with open(SCORES_FILE, "r") as f:
         return json.load(f)
 
 
 def save_score(name, score, distance):
     scores = load_scores()
-    scores.append({"name": name, "score": score, "distance": distance})
-    scores = sorted(scores, key=lambda x: x["score"], reverse=True)[:10]
+    scores.append({"name": name, "score": score, "distance": distance})  #добавление нового игрока
+    scores = sorted(scores, key=lambda x: x["score"], reverse=True)[:10]  #от большего к меньшему топ10
 
     with open(SCORES_FILE, "w") as f:
         json.dump(scores, f)
